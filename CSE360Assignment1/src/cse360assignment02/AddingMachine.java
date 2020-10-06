11869f312c238c97417366f760fbf2b5237a20e7
Rifa Vhora <rvhora@asu.edu>
2020-10-05 18:01:14 -0700

    1st commit with documentation

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assignment02;
+
+/**
+ * Class that can add and subtract numbers to a sum and print a string of all operations
+ *
+ * @author rifav
+ * @version v1
+ */
+public class AddingMachine {
+
+  private int total; // integer variable that will contain the total sum
+  
+  /**
+   * Constructor for the class adding machine
+   * Instantiates variable total which will contain the sum
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Method to return the total
+   * 
+   * @return int total sum
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Method to add the value in the parameter to the total sum
+   *
+   * @param value    the number to be added
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Method to subtract the value in the parameter from the total sum
+   *
+   *@param value	the number to be subtracted
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Method to return a string with all operations performed
+   *
+   *@return a string	string of all operations
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Method to clear the string with all operations performed
+   */
+  public void clear() {
+  }
+
+}
